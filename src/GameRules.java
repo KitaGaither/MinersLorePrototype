a238977d31a150c8c0e2bb5c692fb4444b418f52
KitaGaither <lakitgai@amazon.com>
2022-08-29 19:52:14 -0700

    Added README and Story class

--- src/GameRules.java
+++ src/GameRules.java
@@ -1,4 +1,4 @@
-public class Game {
+public class GameRules {
     public static void main(String[] args) {
 
         System.out.println("Miner's Lore Game" + "\n");
---
+++ src/Story.java
@@ -0,0 +1,8 @@
+public class Story {
+    public static void main(String[] args) {
+        System.out.println(
+                "Miner's Lore takes place in the old west during the biggest gold rush America has ever seen."
+
+        );
+    }
+}
