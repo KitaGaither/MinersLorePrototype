722bed4748a4933bbb78b0bc09c332a8657fae23
KitaGaither <lakitgai@amazon.com>
2022-08-30 08:23:08 -0700

    Updated story

--- src/Story.java
+++ src/Story.java
@@ -1,7 +1,16 @@
 public class Story {
     public static void main(String[] args) {
         System.out.println(
-                "Miner's Lore takes place in the old west during the biggest gold rush America has ever seen."
+                "Miner's Lore takes place in the old west during the biggest gold rush America has ever seen. Poor" +
+                        "miners from all over the world are racing to the west to amass the riches and wealth that " +
+                        "they'd heard about through numerous tales and lore. Although energies are high with wanting" +
+                        "to obtain this gold, many are still a bit worried about the dangers of digging in these " +
+                        "unfamiliar tunnels and caves. Tales about a gnarly little monster called the Knocker have many " +
+                        "hesitant to take the journey. The Knocker's were known for stealing miner's food, gold" +
+                        ", and other important tools. The biggest concern was that Knocker's had the " +
+                        "ability to collapse parts of the gold mine on unsuspecting miners. The greed of many" +
+                        "cost them their lives. Very few have made it out of the mines sucessfully. You've" +
+                        "been warned."
 
         );
     }
